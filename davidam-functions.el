1291e4745d9e356a957b6259094221d62663245d
David Arroyo Menendez <davidam@gmail.com>
2018-01-13 15:05:38 +0100

    davidam-functions.el: replace es.gnu.org to gnu.org

--- davidam-functions.el
+++ davidam-functions.el
@@ -1,7 +1,7 @@
 ;; Copyright (C) 2013  David Arroyo Menéndez
 
-;; Author: David Arroyo Menéndez <davidam@es.gnu.org>
-;; Maintainer: David Arroyo Menéndez <davidam@es.gnu.org>
+;; Author: David Arroyo Menéndez <davidam@gnu.org>
+;; Maintainer: David Arroyo Menéndez <davidam@gnu.org>
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
