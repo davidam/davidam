121ad36878fc840b6c074bd6ffce3a44b296562d
David Arroyo Menendez <davidam@gmail.com>
2018-01-13 14:12:45 +0100

    davidam-conf.el, .emacs: creation

---
+++ davidam-conf.el
@@ -0,0 +1,75 @@
+;; Copyright (C) 2018  David Arroyo Menéndez
+
+;; Author: David Arroyo Menéndez <davidam@gnu.org>
+;; Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+;; Boston, MA 02110-1301 USA,
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(show-paren-mode)
+(display-battery-mode)
+
+;; PERSONAL DATA
+(setq user-full-name "David Arroyo Menendez")
+(setq user-mail-address "davidam@gnu.org")
+(put 'upcase-region 'disabled nil)
+
+;; AUTOCOMPLETE
+(load "/home/davidam/git/popup-el/popup.el")
+(load "/home/davidam/git/auto-complete/auto-complete.el")
+
+;; RUBY
+(add-to-list 'auto-mode-alist
+	     '("\\.\\(?:cap\\|gemspec\\|irbrc\\|gemrc\\|rake\\|rb\\|ru\\|thor\\)\\'" . ruby-mode))
+
+(add-to-list 'auto-mode-alist
+	                    '("\\(?:Brewfile\\|Capfile\\|Gemfile\\(?:\\.[a-zA-Z0-9._-]+\\)?\\|[rR]akefile\\)\\'" . ruby-mode))
+
+;; PYTHON
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)))
+
+;; DRUPAL
+(setq drupal-el-path "~/bzr/drupal-el/")
+(load (concat drupal-el-path "drupal.el"))
+
+;; ORG-MODE	
+(load "~/git/org-mode/contrib/lisp/org-license.el")
+(load "~/git/org-mode/contrib/lisp/org-effectiveness.el")
+
+;;;; PO-MODE ;;;;
+(require 'po-mode)
+(defun iacute()
+(interactive)
+(insert "@'{@dotless{i}}"))
+;; El atajo C-i insertará i acentuada, a la manera nativa de texinfo
+(define-key po-subedit-mode-map "\C-i" 'iacute)
+
+;; Debe cortar las líneas automáticamente
+(add-hook 'po-subedit-mode-hook 'auto-fill-mode)
+
+;;;; ASPELL ;;;;
+;; Para que avise
+(setq ispell-program-name "aspell" ispell-extra-args '("--lang=es"))
+(add-hook 'po-subedit-mode-hook
+	  (lambda()
+	    (flyspell-mode 1)))
+
+;;;; Dictionary ;;;;
+(global-set-key "\C-cs" 'dictionary-search)
+(global-set-key "\C-cm" 'dictionary-match-words)
