1459b7e4fd6d18de495ef30532e97cfa63f6b746
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-08-01 17:37:44 +0200

    davidam-skeletons.el: add davidam-skeleton-js-copyright

--- davidam-skeletons.el
+++ davidam-skeletons.el
@@ -322,3 +322,34 @@
   > ";; <https://github.com/davidam/workingclasslicense>." \n
   )
 
+
+(define-skeleton davidam-skeleton-js-copyright
+  "Add copyright note"
+  ""
+  '(setq program (skeleton-read "Software name? "))
+  > "/*" \n
+  > "        @licstart  The following is the entire license notice for the " \n
+  > "        JavaScript code in this page. " \n
+  > " " \n
+  > "        Copyright (C) " (format-time-string "%Y") "  David Arroyo Menéndez " user-mail-address \n
+  > " " \n
+  > "        The JavaScript code in this page is free software: you can " \n
+  > "        redistribute it and/or modify it under the terms of the GNU " \n
+  > "        General Public License (GNU GPL) as published by the Free Software " \n
+  > "        Foundation, either version 3 of the License, or (at your option) " \n
+  > "        any later version.  The code is distributed WITHOUT ANY WARRANTY; " \n
+  > "        without even the implied warranty of MERCHANTABILITY or FITNESS " \n
+  > "        FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details. " \n
+  > " " \n
+  > "        As additional permission under GNU GPL version 3 section 7, you " \n
+  > "        may distribute non-source (e.g., minimized or compacted) forms of " \n 
+  > "        that code without the copy of the GNU GPL normally required by " \n
+  > "        section 4, provided you include this license notice and a URL " \n
+  > "        through which recipients can access the Corresponding Source. " \n
+  > " " \n
+  > " " \n
+  > "        @licend  The above is the entire license notice " \n
+  > "        for the JavaScript code in this page.  " \n
+  > "*/" \n
+  )
+  
